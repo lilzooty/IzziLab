d452ec8a2296cc66cb4a6193a86f93c001a7d083
gabetevis <gabetevis@gmail.com>
2025-04-08 14:47:54 -0600

    Added toolbar and actions.

--- CircuitBuilder/mainwindow.cpp
+++ CircuitBuilder/mainwindow.cpp
@@ -1,11 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QAction>
+#include <QToolBar>
+#include <QActionGroup>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    QToolBar* gatesToolBar = ui->toolBar;
+
+    QAction* andGate = ui->actionAndGate;
+
+    QAction* orGate = ui->actionOrGate;
+
+    QAction* inverter = ui->actionInverter;
+
+    QAction* wire = ui->actionWire;
+
+    QActionGroup *group = new QActionGroup(this);
+
+    group->addAction(andGate);
+    group->addAction(orGate);
+    group->addAction(inverter);
+    group->addAction(wire);
 }
 
 MainWindow::~MainWindow()
