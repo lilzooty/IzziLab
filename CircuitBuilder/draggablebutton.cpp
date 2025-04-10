bff14aa90dd2d2a483b6b2ea6089102e6ac2b283
sorenfanning <157437000+sorenfanning@users.noreply.github.com>
2025-04-10 12:40:04 -0600

    Addded box 2d and updated draggablebutton.cpp

--- CircuitBuilder/draggablebutton.cpp
+++ CircuitBuilder/draggablebutton.cpp
@@ -1,5 +1,4 @@
 #include "draggablebutton.h"
-#include <QMouseEvent>
 
 DraggableButton::DraggableButton() {
 
@@ -21,11 +20,52 @@ void DraggableButton::mousePressEvent(QMouseEvent *event)
     QPushButton::mousePressEvent(event);
 }
 
+
 void DraggableButton::mouseMoveEvent(QMouseEvent *event)
 {
     if (event->buttons() & Qt::LeftButton) {
-        QPoint diff = event->pos() - dragStartPos;
-        move(pos() + diff);
+
+        QPoint globalPos = mapToParent(event->pos() - dragStartPos);
+        QPoint newPos = snapToGrid(globalPos);
+
+        move(newPos);
+        updatePhysicsBody(newPos);
     }
+
     QPushButton::mouseMoveEvent(event);
+}
+
+QPoint DraggableButton::snapToGrid( QPoint& point)
+{
+    // assume closest Grid Cell is up and left
+    // round to that Grid Cell
+    int relativeX = std::round(point.x() / GRID_SIZE) * GRID_SIZE;
+    int relativeY = std::round(point.y() / GRID_SIZE) * GRID_SIZE;
+
+    // if click closer to right Cell add
+    if (point.x() % GRID_SIZE >  GRID_SIZE/2 ){
+        relativeX += GRID_SIZE;
+    }
+
+    // if click closer to the bottom add
+    if (point.y() % GRID_SIZE >  GRID_SIZE/2 ){
+        relativeY += GRID_SIZE;
+    }
+
+    return QPoint(relativeX, relativeY);
+}
+
+b2Body* DraggableButton::getPhysicsBody()
+{
+    return body;
+}
+
+void DraggableButton::updatePhysicsBody( QPoint& newPos)
+{
+    if (b2Body* body = getPhysicsBody()) {
+        b2Vec2 physicsPos(newPos.x() / PIXELS_PER_METER,
+                          newPos.y() / PIXELS_PER_METER);
+
+        body->SetTransform(physicsPos, body->GetAngle());
+    }
 }
--- CircuitBuilder/draggablebutton.h
+++ CircuitBuilder/draggablebutton.h
@@ -1,9 +1,12 @@
 #ifndef DRAGGABLEBUTTON_H
 #define DRAGGABLEBUTTON_H
 #include <QPushButton>
+#include <QMouseEvent>
 #include <string>
+#include <Box2D/Box2D.h>
 
-class DraggableButton : QPushButton
+
+class DraggableButton : public QPushButton
 {
     Q_OBJECT
 
@@ -11,6 +14,17 @@ private:
 
     QString buttonType;
     QPoint dragStartPos;
+    QPoint currentPos;
+
+    b2Body* body;
+
+    int GRID_SIZE = 50;
+    float PIXELS_PER_METER = 30.0f;
+
+    QPoint snapToGrid(QPoint& point);
+    void updatePhysicsBody(QPoint& newPos);
+    b2Body* getPhysicsBody();
+
 protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
@@ -18,8 +32,10 @@ protected:
 
 public:
     DraggableButton();
-
     explicit DraggableButton(QString buttonType, QWidget *parent = nullptr);
+    QPoint getPosition() { return currentPos; }
+    void setPosition(QPoint& pos) { currentPos = pos; }
+    void setBody(b2Body* newBody) {body = newBody;}
 
 };
 
